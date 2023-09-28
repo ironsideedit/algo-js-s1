e7a40e233afeef9e50fc759951c54d200bd0af59
ironsideedit <146093941+ironsideedit@users.noreply.github.com>
2023-09-28 16:06:59 +0200

    pokemon

---
+++ exo_1/exo_2/class pokemon/mains.js
@@ -0,0 +1,14 @@
+class Raichu {
+    constructor(attack, defence){
+      this.attack = attack;
+      this.defense= defence; 
+      this.hp = hp; 
+    }
+    getAttack(éclaire){
+     console.log(this.attack);
+
+    }
+}
+
+let Alexis = new Raichu
+    
