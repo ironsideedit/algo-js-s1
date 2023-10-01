89af2c732934bcf90d01b1959511a5aea8135ad6
ironsideedit <146093941+ironsideedit@users.noreply.github.com>
2023-10-01 15:49:50 +0200

    rendu 2 abdel

---
+++ exo taxi rendu pour dimanche  abdel/exo tuer/main.js
@@ -0,0 +1,13 @@
+ class Character {
+        constructor (name, hp, degats ){
+          this.name = name
+          this.hp = hp
+          this.degats= degats
+        }
+        while(degats < 100 && this.hp >0 ){
+            console.log ("le personnage a perdu 1 hp")
+}
+let jason = new Character("jason",100)
+let nerd = new Character("nerd",50)
+let blonde = new Character("blonde",50)
+let degat=["nerd","blonde"]
